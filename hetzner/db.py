8f65e3b4880fc99701818dedb4413fa298798e11
Arne Beer <arne@twobeer.de>
2017-12-20 20:45:19 +0100

    Fix import

--- hetzner/db.py
+++ hetzner/db.py
@@ -1,6 +1,7 @@
 """Helper class to get a database engine and to get a session."""
 from hetzner.config import SQL_URI
 from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
