79c7b2059450d3c7614cf7f80a79d94071357159
Arne Beer <contact@arne.beer>
2022-12-26 17:48:03 +0100

    fix: live data url

--- hetznerbot/helper/hetzner.py
+++ hetznerbot/helper/hetzner.py
@@ -24,7 +24,7 @@ def get_hetzner_offers():
 
     # Hetzner expects a millisecond unix timestamp at the end
     unix_timestamp = int(time.time() * 1000)
-    url = f"https://www.hetzner.com/_resources/app/jsondata/live_data_de.json?m={unix_timestamp}"
+    url = f"https://www.hetzner.com/_resources/app/jsondata/live_data_sb.json?m={unix_timestamp}"
     try:
         response = request("GET", url, headers=headers)
         data = json.loads(response.content)
